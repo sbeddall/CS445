459153fe9bc64d26eb9051363bc78fc5b7813571
Scott Beddall <sbeddall@gmail.com>
2011-08-25 22:04:27 -0700

    main.c is completed. working on yylex.c now

--- hw1/main.c
+++ hw1/main.c
@@ -5,8 +5,25 @@ char yytext[1024];
 FILE *yyin;
 
 int main(int argc, char* argv[]){
-  strcpy(yytext, "damn!");
+  int identifier = 0;
+  int i = 1;
   
-  yylex();
+  for(i; i < argc; i++){
+    yyin = fopen( argv[i], "r" );
+    
+ 
+    if(yyin == 0){
+      printf("Could not open file");
+      return (0);
+    }
+    
+    while(identifier != -1){
+      identifier = yylex();
+      printf("%d\t%s\n", identifier, yytext);
+    }
+    
+    fclose( yyin );
+  }
+  
   return 0;
 }
--- hw1/yylex.c
+++ hw1/yylex.c
@@ -3,7 +3,26 @@
 extern char yytext[];
 extern FILE *yyin;
 
-yylex(){
-  yytext[0] = 'a'; 
-  printf("%s\n", yytext);
+//discard spaces tabs and newlines
+//ascii 32, 9, and 10 respectively
+int yylex(){
+  int x;
+  
+  //get first character. check for whitespace
+  x = fgetc( yyin );
+  
+  
+
+  printf("%d\n", x);
+
+  yytext[0] = (char)x;
+  
+  
+  
+  return -1;
 }
+
+int parseIdentifier(){
+  return -1;
+}
+
