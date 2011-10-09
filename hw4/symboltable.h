6d1aa688b5d6558e3f214afae3566e5b1fe3fe93
Scott Beddall <sbeddall@gmail.com>
2011-10-08 22:41:58 -0700

    a commit before I rip my tree apart

--- hw4/symboltable.h
+++ hw4/symboltable.h
@@ -1,6 +1,25 @@
 #ifndef SYMBOL_TABLE
 #define SYMBOL_TABLE
-
-
-
+struct c_type {
+  int base_type;    /* 1 = int, 2=float, ... */
+  union {
+    struct array {
+      int size;
+      struct c_type *elemtype;
+    } a;
+    struct ctype *p;
+    struct struc {
+      char *label;
+      struct field **f;
+    } s;
+  } u;
+}
+  
+struct field {
+  char *name;
+  struct ctype *elemtype;
+}
+  
+struct symbol_table { 
+}
 #endif
